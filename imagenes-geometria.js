049cba7a8cdb710953e79826c516d3d4537b7b73
IMP735-code <huaccachip735@gmail.com>
2025-12-16 12:32:07 -0500

    Create imagenes-geometria.js

---
+++ imagenes-geometria.js
@@ -0,0 +1,14 @@
+const imagenesGeometria = [
+  {
+    archivo: "problema-geometria-1.png",
+    texto: "Geometría analítica, paraábola, longitud máxima."
+  },
+  {
+    archivo: "funciones.png",
+    texto: "Funciones algebraicas"
+  },
+  {
+    archivo: "matrices.png",
+    texto: "Matrices y operaciones"
+  }
+];
