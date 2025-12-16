1542628d9f07e0d471ea633b71c66c3db40e5d2b
IMP735-code <huaccachip735@gmail.com>
2025-12-16 07:31:50 -0500

    Create imagenes-algebra.js

---
+++ imagenes-algebra.js
@@ -0,0 +1,14 @@
+const imagenesAlgebra = [
+  {
+    archivo: "ecuacion-radicacion.png",
+    texto: "Ecuación con radicación paso a paso"
+  },
+  {
+    archivo: "funciones.png",
+    texto: "Funciones algebraicas"
+  },
+  {
+    archivo: "matrices.png",
+    texto: "Matrices y operaciones"
+  }
+];
