ddc89641d166e72987413fb88aa4609bfe7daad7
IMP735-code <huaccachip735@gmail.com>
2025-12-18 12:55:46 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// config.js - Centro de control de tus imágenes
+const GALERIA_CONFIG = {
+    // Estas 4 imágenes aparecerán en la sección "Problemas recientes" del index.html
+    recientes: [
+        "recientes/problema1.png",
+        "recientes/problema2.png",
+        "recientes/problema3.png",
+        "recientes/problema4.png"
+    ],
+
+    // Todas estas aparecerán en todos-problemas.html (puedes agregar muchísimas)
+    todos: [
+        "todos/problema001.png",
+        "todos/problema002.png",
+        "todos/problema003.png",
+        "todos/problema004.png",
+        "todos/problema005.png",
+        "todos/problema006.png"
+    ]
+};
